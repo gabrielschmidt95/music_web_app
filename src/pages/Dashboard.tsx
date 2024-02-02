9cba2eab49869be46ff398cd16eabc0217ec6cb9
Gabriel Schmidt <gabrieloliveira95@live.com>
2024-02-02 14:57:44 -0300

    Update user authentication logic

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -74,62 +74,53 @@ const Dashboard: React.FunctionComponent = () => {
                     overflowY: 'auto',
                 }
             }>
-                <Row>
-                    <Col>
-                        <h1>Dashboard</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={5}>
-                        <Table>
-                            <thead>
-                                <tr>
-                                    <th>Media</th>
-                                    <th>Quantidade</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    totals ? Object.keys(totals.media).map(key => {
-                                        return <tr key={key}>
-                                            <td>{key}</td>
-                                            <td>{totals.media[key]}</td>
-                                        </tr>
-                                    }) : <></>
-                                }
-                            </tbody>
-                        </Table>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h2>Compras por Ano</h2>
-                        <Bar data={purchaseByYear} options={{
-                            responsive: true,
-                            plugins: {
-                                legend: {
-                                    position: 'top' as const,
-                                },
-                            },
-                            onClick: (evt: any, item: any) => {
-                                if (item[0] !== undefined) {
-                                    const year = Object.keys(purchaseByYear.datasets[0].data!)[item[0].index]
-                                    FetchAlbumsByYearMetric(parseInt(year), "purchase").then((data) => {
-                                        setModalYear(parseInt(year))
-                                        setModalValue(data)
-                                        handleShowModal()
-                                    })
-
-                                }
-                            }
-                        }} />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <h2>Lançamentos por Ano</h2>
-                        <Bar data={releaseByYear} options={
+                <Col>
+                    <Row>
+                        <Col>
+                            <h1>Dashboard</h1>
+                        </Col>
+                    </Row>
+                    <Row
+                        style={
                             {
+                                padding: '2rem',
+                                boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+                                borderRadius: '1rem',
+                            }
+                        }
+                    >
+                        <Col xs={5}>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th>Media</th>
+                                        <th>Quantidade</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        totals ? Object.keys(totals.media).map(key => {
+                                            return <tr key={key}>
+                                                <td>{key}</td>
+                                                <td>{totals.media[key]}</td>
+                                            </tr>
+                                        }) : <></>
+                                    }
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
+                    <br />
+                    <Row>
+                        <Col
+                            style={{
+                                padding: '2rem',
+                                boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+                                borderRadius: '1rem',
+                            }}
+                        >
+                            <h2>Compras por Ano</h2>
+                            <Bar data={purchaseByYear} options={{
                                 responsive: true,
                                 plugins: {
                                     legend: {
@@ -138,8 +129,8 @@ const Dashboard: React.FunctionComponent = () => {
                                 },
                                 onClick: (evt: any, item: any) => {
                                     if (item[0] !== undefined) {
-                                        const year = Object.keys(releaseByYear.datasets[0].data!)[item[0].index]
-                                        FetchAlbumsByYearMetric(parseInt(year), "release_year").then((data) => {
+                                        const year = Object.keys(purchaseByYear.datasets[0].data!)[item[0].index]
+                                        FetchAlbumsByYearMetric(parseInt(year), "purchase").then((data) => {
                                             setModalYear(parseInt(year))
                                             setModalValue(data)
                                             handleShowModal()
@@ -147,10 +138,43 @@ const Dashboard: React.FunctionComponent = () => {
 
                                     }
                                 }
+                            }} />
+                        </Col>
+     
+                        <Col
+                            style={
+                                {
+                                    padding: '2rem',
+                                    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+                                    borderRadius: '1rem',
+                                }
                             }
-                        } />
-                    </Col>
-                </Row>
+                        >
+                            <h2>Lançamentos por Ano</h2>
+                            <Bar data={releaseByYear} options={
+                                {
+                                    responsive: true,
+                                    plugins: {
+                                        legend: {
+                                            position: 'top' as const,
+                                        },
+                                    },
+                                    onClick: (evt: any, item: any) => {
+                                        if (item[0] !== undefined) {
+                                            const year = Object.keys(releaseByYear.datasets[0].data!)[item[0].index]
+                                            FetchAlbumsByYearMetric(parseInt(year), "release_year").then((data) => {
+                                                setModalYear(parseInt(year))
+                                                setModalValue(data)
+                                                handleShowModal()
+                                            })
+
+                                        }
+                                    }
+                                }
+                            } />
+                        </Col>
+                    </Row>
+                </Col>
             </Container>
             <Modal show={showModal} onHide={handleCloseModal} size="xl" >
                 <Modal.Header closeButton>
@@ -168,15 +192,15 @@ const Dashboard: React.FunctionComponent = () => {
                         </thead>
                         <tbody>
                             {
-                                modalValue? 
-                                modalValue.map((album, _) => {
-                                    return <tr key={album.title}>
-                                        <td>{album.artist}</td>
-                                        <td>{album.title}</td>
-                                        <td>{album.media}</td>
-                                        <td>{album.purchase ? album.purchase.split("-")[2] + "/" + album.purchase.split("-")[1]+"/" +album.purchase.split("-")[0]: ""}</td>
-                                    </tr>
-                                }) : <></>
+                                modalValue ?
+                                    modalValue.map((album, _) => {
+                                        return <tr key={album.title}>
+                                            <td>{album.artist}</td>
+                                            <td>{album.title}</td>
+                                            <td>{album.media}</td>
+                                            <td>{album.purchase ? album.purchase.split("-")[2] + "/" + album.purchase.split("-")[1] + "/" + album.purchase.split("-")[0] : ""}</td>
+                                        </tr>
+                                    }) : <></>
                             }
                         </tbody>
                     </Table>
